7ef01fd85daf6d03bfe8757486e85b320062c776
habben57 <137295254+habben57@users.noreply.github.com>
2023-06-29 15:37:32 +0200

    jalon

---
+++ Algo-Java/exemple/src/AdresseMail.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class AdresseMail {
+
+	public static void main(String[] args) {
+		/*
+		 VARIABLE
+		    Prénom est un entier
+		    Nom est un entier
+		    CodePostal est un entier
+		    AdresseMail est un In
+		Debut programme
+		    Ecrire "Saisir votre Prénom"
+		    Lire <-- Prénom
+		    Ecrire "Saisir votre Nom"
+		    Lire <-- Nom
+		    Ecrire "Saisir Code Postal"
+		    Lire <-- Code Postal
+		    Resultat <-- Prénom + Nom + Code Postal + @arfp.asso.fr
+		    Ecrire "le résultat de L adresse Mail pour ",Prenom, Nom, Code Postal, @arfp.asso.fr, " ou " Resultat
+		Fin programme
+		 */
+	
+	public class App {
+
+			public static void main(String[] args) {
+				// declaration des variables
+				
+				Prenom est un string
+			    String Nom 
+			    String CodePostal 
+				Scanner sc;
+				sc = new Scanner(System.in);
+				//Scanner sc = new Scanner(System.in);
+				
+				System.out.println("Saisir votre Prénom ");
+				Prénom = sc.nextInt();
+				
+				System.out.println("Saisir votre Nom ");
+				Nom = sc.nextInt();
+				
+				System.out.println("Saisir votre Code Postal ");
+				CodePostal = sc.println();
+				
+				resultat1 = Prenom + Nom + CodePostal + @arfp.asso.fr ;
+				resultat2 = Prenom + CodePostal + Nom +  + @arfp.asso.fr ;
+				
+				System.out.println("Votre adresse Mail est "+
+						Prenom + Nom + CodePostal + @arfp.asso.fr + " ou " Prenom + CodePostal + Nom +  @arfp.asso.fr + 
+									 
+									resultat);
+				
+				System.out.println("Le resultat de l'addition de "+
+						nombre1 +" et "+ nombre2 + " est "+
+						resultat);
+				
+				
+				sc.close();
+
+	}
+
+}
