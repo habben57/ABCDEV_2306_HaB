0ba08199491926232a7420f13ad943f10249bc8a
habben57 <137295254+habben57@users.noreply.github.com>
2023-09-07 15:43:21 +0200

    Voiture

--- Algo-Java/Exercices_algo/src/ObjetVoiture/App.java
+++ Algo-Java/Exercices_algo/src/ObjetVoiture/App.java
@@ -4,12 +4,21 @@ public class App {
 
 	public static void main(String[] args) 
 
-		{
-			Voiture maVoiture = new Voiture();
-			maVoiture.setMarque(" Renault Laguna");
-			maVoiture.setPrix(  24000.0);
-			maVoiture.Afficher();
-		}
-	
+//		{
+//			Voiture maVoiture = new Voiture();
+//			maVoiture.setMarque(" Renault Laguna");
+//			maVoiture.setPrix(  24000.0);
+//			maVoiture.Afficher();
+//		}
+	{
+		Voiture maVoiture1 = new Voiture();
+		Voiture maVoiture2 = new Voiture();
+		maVoiture1.setMarque(" Renault Laguna");
+		maVoiture1.setPrix(  24000.0);
+		maVoiture1.Afficher();
+		maVoiture2.setPrix(  21000.0);
+		maVoiture2.setMarque( " Peugeot 205 ");
+		maVoiture2.Afficher();
+	}
 
 }
